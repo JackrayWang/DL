90e7f5fbd779cd43a457c461d452fcafa8c5e598
Jackray <jackray_wang@sina.com>
2018-10-30 00:36:52 +0800

    log4j2

---
+++ src/main/java/com/spring/bussiness/dao/User.java
@@ -0,0 +1,28 @@
+package com.spring.bussiness.dao;
+
+public class User {
+	private String username;
+	private String sex;
+	private String nick;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
+
+
+}
